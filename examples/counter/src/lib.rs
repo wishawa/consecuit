fc97a4331da9e0c0a916d3e77ca3fe103060ba23
Wisha Wa <wisha.wa@yandex.com>
2021-06-15 19:25:33 +0000

    remove unused alert

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -5,11 +5,6 @@ use reia::{
 };
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
 #[wasm_bindgen(start)]
 pub fn run() -> Result<(), JsValue> {
     println!("Hello, world!");
